eb58d6713197920d680d7bafcec10a296192e793
wanlong <546278202@qq.com>
2018-10-12 16:47:06 +0800

    window

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,52 @@
+<template>
+    <section>
+        <div der="" class="top-naver" style="display:flex;height: 50px;align-items: center;border-bottom: 1px solid #ccc;background: #fff;">
+           <a class="top-right" id="show-msg" style="width:45px;">
+                <i class="iconfont icon-xiaoxi" style="font-size: 1rem ;"></i>
+                <p style="font-size: 0.6rem ;line-height: 0.25rem ;">消息</p>
+            </a>
+            <div class="searchInput">
+                <i class="iconfont icon-iconfontzhizuobiaozhun22" style="font-size: 1rem  ;position: absolute;left: 0.6rem ;top: 0.5rem ;"></i>
+                <input type="text" placeholder="请输入商品名称" id="SearchInput" class="form-control" name="mobile" style="width: 100%;border:0;">
+            </div>
+           <a class="top-right" id="show-msg" style="width:45px;">
+                <i class="iconfont icon-xiaoxi" style="font-size: 1rem ;"></i>
+                <p style="font-size: 0.6rem ;line-height: 0.25rem ;">消息</p>
+            </a>
+        </div>
+    </section>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data () {
+        return {
+            msg: ''
+        }
+    },
+  mounted(){
+      axios.post('http://47.93.4.157:8086/mall_api/shop/get_ware_list', {
+          firstName: 'Fred',
+          lastName: 'Flintstone'
+      })
+      .then(function (response) {
+          console.log(response);
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
+  }
+}
+</script>
+<style scoped>
+    .searchInput{
+        position: relative;
+        border: 1px solid #ccc;
+        height: 30px;
+        border-radius: 30px;
+        overflow: hidden;
+        line-height: 30px;
+        flex:1;
+        padding: 0 10px;
+    }
+</style>
---
+++ src/page/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <SearchBar></SearchBar>
+        <!-- 轮播 -->
+        <SwiperBar></SwiperBar>
+    </div>   
+</template>
+
+<script>
+import axios from 'axios'
+import SearchBar from '../components/SearchBar'
+import SwiperBar from '../components/SwiperBar'
+export default {
+    data () {
+        return {
+        }
+    },
+    mounted(){
+        axios.post('http://47.93.4.157:8086/mall_api/shop/get_ware_list', {
+            firstName: 'Fred',
+            lastName: 'Flintstone'
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    },
+    components:{
+        SearchBar,
+        SwiperBar
+    },
+}
+</script>
+<style scoped>
+
+
+</style>
